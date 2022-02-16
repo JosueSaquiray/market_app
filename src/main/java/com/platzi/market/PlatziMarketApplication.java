35936d828ffbd53d1a9ece95ffcd7fe30770b47a
Josue Saquiray <josue.saquiray@pucp.edu.pe>
2022-02-16 01:04:59 -0500

    No sé que está pasando, acabo de añadir archivos que se suponia ya estaban pero no

---
+++ src/main/java/com/platzi/market/PlatziMarketApplication.java
@@ -0,0 +1,15 @@
+package com.platzi.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+@SpringBootApplication
+public class PlatziMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMarketApplication.class, args);
+	}
+
+
+}
